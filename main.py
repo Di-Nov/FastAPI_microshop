30a68d269559169d20a255e0f55858873d234030
dima <pieero47@gmail.com>
2024-06-15 16:03:19 +0300

    update main

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from contextlib import asynccontextmanager
-
 import uvicorn
 from fastapi import FastAPI
-from core.models import DatabaseHelper, db_helper, Base, Product
-
 from api.v1.routers_v1 import routers_v1
 from core.config import settings
 
@@ -12,8 +9,6 @@ from core.config import settings
 async def lifespan(app: FastAPI):
     """Менеджер для создания таблиц перед запуском приложения.
     Выполнения каких-то действий после закрытия прилодения (После yieald)"""
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
