95fc855dfb6f894d7456ff7ba67e4a8cec39c899
dima <pieero47@gmail.com>
2024-06-15 19:46:16 +0300

    create relationship between user and post

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,4 +1,14 @@
-__all__ = ("Base", "Product", "db_helper", "DatabaseHelper")
-from .base import Base
-from .product import Product
-from .db_helper import db_helper, DatabaseHelper
+__all__ = [
+    "User",
+    "Post",
+    "Product",
+    "Base",
+    "db_helper",
+    "DatabaseHelper",
+]
+
+from core.models.user import User
+from core.models.post import Post
+from core.models.product import Product
+from core.models.base import Base
+from core.models.db_helper import db_helper, DatabaseHelper
