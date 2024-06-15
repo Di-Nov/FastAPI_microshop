ef11d308c3129803fcf47308972664d2c28084da
dima <pieero47@gmail.com>
2024-06-15 21:38:56 +0300

    create user_relation_mixin

--- core/models/post.py
+++ core/models/post.py
@@ -1,17 +1,16 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, Text
+from sqlalchemy import Text, String
 from typing import TYPE_CHECKING
 
-from core.models import Base
+from core.models.base import Base
+from core.models.mixins.user_relation_mixin import UserRelationMixin
 
 if TYPE_CHECKING:
     from core.models import User
 
 
-class Post(Base):
-    title: Mapped[str] = mapped_column()
+class Post(UserRelationMixin, Base):
+    _user_back_populates = "posts"
+
+    title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("user.id"),
-    )
-    user: Mapped["User"] = relationship(back_populates="posts")
