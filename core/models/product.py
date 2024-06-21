8d9e669f0fcef28cc977a4a69472a0f9004ef477
dima <pieero47@gmail.com>
2024-06-21 16:23:23 +0300

    add relationship in Product model

--- core/models/product.py
+++ core/models/product.py
@@ -1,9 +1,16 @@
-from sqlalchemy.orm import Mapped
-
+from sqlalchemy.orm import Mapped, relationship
+from typing import TYPE_CHECKING
 from .base import Base
+
+if TYPE_CHECKING:
+    from .order import Order
 
 
 class Product(Base):
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[int]
+
+    orders: Mapped[list["Order"]] = relationship(
+        secondary="order_product_association", back_populates="products"
+    )
