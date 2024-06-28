697128b87a5ea0d365ccc710f9f28e89c636eb58
dima <pieero47@gmail.com>
2024-06-28 14:22:26 +0300

    change order_product_association model, add new rows (count, unit_price, order, product)

--- core/models/order_product_association.py
+++ core/models/order_product_association.py
@@ -1,7 +1,12 @@
 from sqlalchemy import Table, Column, ForeignKey, Integer, UniqueConstraint
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
 
 from core.models.base import Base
+
+if TYPE_CHECKING:
+    from .order import Order
+    from .product import Product
 
 
 class OrderProductAssociation(Base):
@@ -13,6 +18,12 @@ class OrderProductAssociation(Base):
 
     order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"))
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
+
+    count: Mapped[int] = mapped_column(default=1, server_default="1")
+    unit_price: Mapped[int] = mapped_column(default=0, server_default="0")
+
+    order: Mapped["Order"] = relationship(back_populates="products_details")
+    product: Mapped["Product"] = relationship(back_populates="orders_details")
 
 
 # order_product_association_table = Table(
