32e229ee8b73d2a748bea003662acf2e1f6bf9f4
dima <pieero47@gmail.com>
2024-07-04 18:44:06 +0300

    Create scheme AuthJWT

--- core/config.py
+++ core/config.py
@@ -10,13 +10,21 @@ DB_PATH = BASE_DIR / "db.sqlite3"
 
 class DbSettings(BaseModel):
     url: str = f"sqlite+aiosqlite:///{DB_PATH}"
-    # echo: bool = False
-    echo: bool = True  # Для вывода логов SQL при создании, только при отладке !!
+    echo: bool = False
+    # echo: bool = True  # Для вывода логов SQL при создании, только при отладке !!
+
+
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 15
 
 
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
     db: DbSettings = DbSettings()
+    auth_jwt: AuthJWT = AuthJWT()
 
 
 settings = Settings()
