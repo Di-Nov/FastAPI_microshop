a6c9d28088a269d657f3fff7964be2196d95a61c
dima <pieero47@gmail.com>
2024-07-04 18:45:35 +0300

    create UserSchema

--- api/v1/users/schemas_users.py
+++ api/v1/users/schemas_users.py
@@ -1,5 +1,5 @@
 from annotated_types import MinLen, MaxLen
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
 from typing import Annotated
 
 
@@ -9,3 +9,13 @@ class CreateUser(BaseModel):
     # username: str = Field(max_length=20, min_length=3)
     email: EmailStr
     password: Annotated[str, MinLen(3), MaxLen(20)]
+
+
+class UserSchema(BaseModel):
+    # Должны быть указаны строго те типы, что мы передали в схему.
+    model_config = ConfigDict(strict=True)
+
+    username: str
+    password: bytes
+    email: str | None = None
+    active: bool = True
